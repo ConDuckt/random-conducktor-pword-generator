17a31ac1be4b7eadddccaa99f74f9dc0dfa18c0b
AMiro <aj.miro.b@gmail.com>
2023-02-13 14:57:02 -0600

    more debugging

--- Assets/script.js
+++ Assets/script.js
@@ -1,63 +1,67 @@
 var generateBtn = document.querySelector("#generate");
 
-var lowerCharacters = 'abcdefjhijklmnopqrstuvwxyz';
-var lowerArray = lowerCharacters.split('');
-var randomLower = lowerArray[Math.floor(lowerArray.length * Math.random())];
+// var lowerCharacters = 'abcdefjhijklmnopqrstuvwxyz';
+// var lowerArray = lowerCharacters.split('');
+// var randomLower = lowerArray[Math.floor(lowerArray.length * Math.random())];
 
-var upperCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var upperArray = upperCharacters.split('');
-var randomUpper = upperArray[Math.floor(upperArray.length * Math.random())];
+// var upperCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var upperArray = upperCharacters.split('');
+// var randomUpper = upperArray[Math.floor(upperArray.length * Math.random())];
 
-var numericCharacters = '0123456789';
-var numericArray = numericCharacters.split('');
-var randomNumeric = numericArray[Math.floor(numericArray.length * Math.random())];
+// var numericCharacters = '0123456789';
+// var numericArray = numericCharacters.split('');
+// var randomNumeric = numericArray[Math.floor(numericArray.length * Math.random())];
 
-var specialCharacters = '!@#$%^&*()[]{}/?-=+_<>~,.;:`\|"';
-var specialArray = specialCharacters.split('');
-var randomSpecial = specialArray[Math.floor(specialArray.length * Math.random())];
+// var specialCharacters = '!@#$%^&*()[]{}/?-=+_<>~,.;:`\|"';
+// var specialArray = specialCharacters.split('');
+// var randomSpecial = specialArray[Math.floor(specialArray.length * Math.random())];
 
-randomArray =
-  randomLower,
-  randomUpper,
-  randomNumeric,
-  randomSpecial;
+// randomArray = '';
+//   lowerCharacters,
+//   upperCharacters,
+//   numericCharacters,
+//   specialCharacters;
+
+allCharacters = {
+  lowerCharacters: 'abcdefjhijklmnopqrstuvwxyz',
+  upperCharacters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numericCharacters: '0123456789',
+  specialCharacters: '!@#$%^&*()[]{}/?-=+_<>~,.;:`\|"',
+};
 
 function generatePassword() {
 
-  var randomAll = '';
+  var randomizer = '';
 
   var passwordLength = prompt("Please enter desired password length:", "8-128 characters.");
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length (8-128 characters.)");
   } else {
-    var randomLower = confirm('Use lowercase letters?')
-      if (randomLower) {
-        randomAll += randomArray.randomLower;
+    var lowerCharacters = confirm('Use lowercase letters?')
+      if (lowerCharacters) {
+        randomizer += allCharacters.lowerCharacters;
       }
-    var randomUpper = confirm('Use uppercase letters?')
-    if (randomUpper) {
-      randomAll += randomArray.randomUpper;
+    var upperCharacters = confirm('Use uppercase letters?')
+    if (upperCharacters) {
+      randomizer += allCharacters.upperCharacters;
     }
-    var randomNumeric = confirm('Use numbers?')
-    if (randomNumeric) {
-      randomAll += randomArray.randomNumeric;
+    var numericCharacters = confirm('Use numbers?')
+    if (numericCharacters) {
+      randomizer += allCharacters.numericCharacters;
     }
-    var randomSpecial = confirm('Use special characters?')
-    if (randomSpecial) {
-      randomAll += randomArray.randomSpecial;
+    var specialCharacters = confirm('Use special characters?')
+    if (specialCharacters) {
+      randomizer += allCharacters.specialCharacters;
     }
 
     var password = ''; {
-      for (var i = 0, i < randomAll.length, i++);
-      password += randomArray;
+      for (var passwordLength = 0, passwordLength < length, passwordLength++);
+      password += randomizer[Math.floor(randomAll.length * Math.random())];;
     }
-    
-    return password;
-
-    }
-
   }
+    return password;
+}
 
 
 function writePassword() {
@@ -67,5 +71,3 @@ function writePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-console.log(generatePassword())
